ef2f910b169bea1b882e25e5d40b6305cae57cd8
Benjamin <milk500ted939@gmail.com>
2017-04-29 11:52:07 +0800

    delete

--- init.sh
+++
@@ -1,7 +0,0 @@
-chmod 777 golden/pipeline
-cd simulator
-sh cr.sh
-cd ../testcase
-g++ -std=c++11 d_generator.cpp -o d_generator
-g++ -std=c++11 i_generator.cpp -o i_generator
-cd ..
--- simulator/cr.sh
+++
@@ -1,4 +0,0 @@
-g++ -std=c++11 *.cpp *.hpp -O2 -c
-g++ -std=c++11 *.o -O2 -o pipeline
-mv pipeline.exe pipeline
-./pipeline
